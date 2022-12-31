62e619a93a80471bfbc1cb28849f67a2f3c694b0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-31 16:40:37 +0000

    Deploying to gh-pages from @ LTLA/millijson@8f2f005fc5b5dc399c7d43d2854c85b42a84ed80 🚀

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['validate_0',['validate',['../namespacemillijson.html#a94ef0ffe025b1b9b0aac8eff2dd5d78b',1,'millijson']]],
-  ['validate_5ffile_1',['validate_file',['../namespacemillijson.html#a09f423462cc924aea20dc9c0e9906a2b',1,'millijson']]],
-  ['validate_5fstring_2',['validate_string',['../namespacemillijson.html#aa8c63ec367806c33225eb5f3da095839',1,'millijson']]],
+  ['validate_0',['validate',['../namespacemillijson.html#a551d822d9494cc6f69dd21d2295d5d13',1,'millijson']]],
+  ['validate_5ffile_1',['validate_file',['../namespacemillijson.html#a87b9da0b860b13582e79779e161506e6',1,'millijson']]],
+  ['validate_5fstring_2',['validate_string',['../namespacemillijson.html#a9ba75129059e2161877310cc4d947ff1',1,'millijson']]],
   ['value_3',['value',['../structmillijson_1_1Number.html#ac69be2fd962eab06d6460bc8f73d0929',1,'millijson::Number::value()'],['../structmillijson_1_1String.html#aa8c4a4911002411aa2a7a142181b284e',1,'millijson::String::value()'],['../structmillijson_1_1Boolean.html#a1fd5e79b36732781997535f71aea865c',1,'millijson::Boolean::value()']]],
   ['values_4',['values',['../structmillijson_1_1Array.html#afbc248664b1e22a1b8b7d6b01281d2ed',1,'millijson::Array::values()'],['../structmillijson_1_1Object.html#a2b44d4a526076630f5f76e45e48261e4',1,'millijson::Object::values()']]]
 ];
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['validate_0',['validate',['../namespacemillijson.html#a94ef0ffe025b1b9b0aac8eff2dd5d78b',1,'millijson']]],
-  ['validate_5ffile_1',['validate_file',['../namespacemillijson.html#a09f423462cc924aea20dc9c0e9906a2b',1,'millijson']]],
-  ['validate_5fstring_2',['validate_string',['../namespacemillijson.html#aa8c63ec367806c33225eb5f3da095839',1,'millijson']]]
+  ['validate_0',['validate',['../namespacemillijson.html#a551d822d9494cc6f69dd21d2295d5d13',1,'millijson']]],
+  ['validate_5ffile_1',['validate_file',['../namespacemillijson.html#a87b9da0b860b13582e79779e161506e6',1,'millijson']]],
+  ['validate_5fstring_2',['validate_string',['../namespacemillijson.html#a9ba75129059e2161877310cc4d947ff1',1,'millijson']]]
 ];
